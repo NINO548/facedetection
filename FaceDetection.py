48263248d34aeabb8a8afd653b6cd15e104f2ac1
NINO548 <87984594+NINO548@users.noreply.github.com>
2021-10-07 14:08:48 +0700

    Add files via upload

---
+++ FaceDetection.py
@@ -0,0 +1,81 @@
+import cv2
+from random import randrange
+  
+# Load some pre-trained dât on face frontals from opencv ( haar cascade algorithm )
+trained_face_data = cv2.CascadeClassifier( cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Choose an image to detect faces in 
+#img = cv2.imread('AnhMinh.jpg')
+
+###capture video from webcam
+webcam = cv2.VideoCapture(0)
+
+### Iterate forever over frames
+while True:
+    ### Read the curent frame
+    successful_frame_read, frame = webcam.read()
+    # Make it black-White( convert grayscale )
+    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+
+
+
+    # Detect faces
+    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+
+    # Draw retangles around the face
+    for (x, y, w, h) in face_coordinates:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0 , 225 , 0), 3)
+
+
+    #print(face_coordinates)
+
+
+    # Display the image and name
+    cv2.imshow('Clever Promgrammer Face Detector',frame)
+
+
+    # Wait to display
+    cv2.waitKey(1)
+
+
+
+
+
+
+
+
+
+"""
+# Make it black-White( convert grayscale )
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+
+
+
+# Detect faces
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+
+# Draw retangles around the face
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (randrange(256), randrange (256),randrange(256)), 10)
+
+
+
+#print(face_coordinates)
+
+
+# Display the image and name
+cv2.imshow('Clever Promgrammer Face Detector',img)
+
+
+# Wait to display
+cv2.waitKey()
+
+
+print("Code complete")
+"""
