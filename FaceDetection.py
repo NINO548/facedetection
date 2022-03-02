5ec3311b8917ca8f056e01badf26960e03fe5fae
NINO548 <87984594+NINO548@users.noreply.github.com>
2022-03-02 22:43:17 +0700

    Update FaceDetection.py

--- FaceDetection.py
+++ FaceDetection.py
@@ -5,50 +5,9 @@ from random import randrange
 trained_face_data = cv2.CascadeClassifier( cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # Choose an image to detect faces in 
-#img = cv2.imread('AnhMinh.jpg')
-
-###capture video from webcam
-webcam = cv2.VideoCapture(0)
-
-### Iterate forever over frames
-while True:
-    ### Read the curent frame
-    successful_frame_read, frame = webcam.read()
-    # Make it black-White( convert grayscale )
-    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+img = cv2.imread(r'C:\Users\NINO\Desktop\Python\AI\FaceDetection\Iron Man.jpg')
 
 
-
-
-
-    # Detect faces
-    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
-
-
-    # Draw retangles around the face
-    for (x, y, w, h) in face_coordinates:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0 , 225 , 0), 3)
-
-
-    #print(face_coordinates)
-
-
-    # Display the image and name
-    cv2.imshow('Clever Promgrammer Face Detector',frame)
-
-
-    # Wait to display
-    cv2.waitKey(1)
-
-
-
-
-
-
-
-
-
-"""
 # Make it black-White( convert grayscale )
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -78,4 +37,3 @@ cv2.waitKey()
 
 
 print("Code complete")
-"""
